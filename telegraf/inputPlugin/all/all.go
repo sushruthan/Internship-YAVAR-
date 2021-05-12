2294330f115721fad1565de6afddf88b91055541
sushruthan <54973988+sushruthan@users.noreply.github.com>
2021-05-12 20:55:03 +0530

    Create all.go

---
+++ telegraf/inputPlugin/all/all.go
@@ -0,0 +1,8 @@
+package all
+
+import (
+	//Blank imports for plugins to register themselves
+	_ "github.com/influxdata/telegraf/plugins/inputs/randomnumber"
+	_ "github.com/influxdata/telegraf/plugins/inputs/trig"
+
+)
